af9e3430470fbf28eb7efa6859458af0f55c83cb
portdevil <portdevil@gmail.com>
2015-07-10 13:13:40 -0700

    Having trouble searching Twitter now and sending pic back to watch.

--- mobile/src/main/java/com/example/jonathan/wearapp/TwitterActivity.java
+++ mobile/src/main/java/com/example/jonathan/wearapp/TwitterActivity.java
@@ -8,6 +8,8 @@ import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.wearable.Wearable;
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
@@ -27,15 +29,20 @@ import com.twitter.sdk.android.tweetui.CompactTweetView;
 import com.twitter.sdk.android.tweetui.TweetViewFetchAdapter;
 
 import java.io.File;
-
+import java.util.concurrent.TimeUnit;
+import android.content.Context;
 
 public class TwitterActivity extends Activity {
+
+    String wearNodeId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_twitter);
         String imagePath = getIntent().getStringExtra("imagePath");
+        wearNodeId = getIntent().getStringExtra("wearNodeId");
+        Log.i("TwitterActivity", "The wearNodeId is " + wearNodeId);
         Log.i("TwitterActivity", "Booting up Twitter.");
         Log.i("TwitterActivity", "Trying to get " + imagePath);
         File imageFile = new File(imagePath);
@@ -49,6 +56,8 @@ public class TwitterActivity extends Activity {
         final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter(this, searchTimeline);
         //setListAdapter(adapter);
 
+        replyToWear("reply to wear");
+
         TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
         final SearchService service = twitterApiClient.getSearchService();
         service.tweets("#cs160excited", null, null, null, "recent", 1, null, null, null, true, new Callback<Search>() {
@@ -57,27 +66,22 @@ public class TwitterActivity extends Activity {
                 Log.i("TwitterActivity", "It thinks it successfully searched.");
                 Log.i("TwitterActivity", "Result: " + result.data.toString());
                 Log.i("TwitterActivity", "Result: " + result.data.tweets.get(0).text);
-                Log.i("TwitterActivity", "Result: " + result.data.tweets.get(0).entities.media.get(0).mediaUrl);
+                Log.i("TwitterActivity", "Result: " + result.data.tweets.get(0).entities.media.get(0).mediaUrl); // This is the right image URL.
             }
 
             public void failure(TwitterException exception) {
-                //Do something on failure
+                Log.i("TwitterActivity", "Exception: " + exception.toString());
+                Log.i("TwitterActivity", "Failed to search for some reason.");
             }
         });
+    }
 
-        /**StatusesService statusesService = Twitter.getApiClient().getStatusesService();
-        statusesService.show(524971209851543553L, null, null, null, new Callback<Tweet>() {
-            @Override
-            public void success(Result<Tweet> result) {
-                Log.i("TwitterActivity", "It thinks it successfully showed a status.");
-                Log.i("TwitterActivity", "Result: " + result.data.text);
-            }
-
-            public void failure(TwitterException exception) {
-                //Do something on failure
-            }
-        });**/
-
+    private void replyToWear(String message) {
+        GoogleApiClient client = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .build();
+        Log.i("TwitterActivity", "Trying to send the reply to wear.");
+        Wearable.MessageApi.sendMessage(client, wearNodeId, "reply to wear", null);
     }
 
     @Override
--- wear/src/main/java/com/example/jonathan/wearapp/WearListenerService.java
+++ wear/src/main/java/com/example/jonathan/wearapp/WearListenerService.java
@@ -1,32 +1,29 @@
 package com.example.jonathan.wearapp;
 
+import android.app.Service;
 import android.content.Intent;
+import android.os.IBinder;
 import android.util.Log;
 
 import com.google.android.gms.wearable.MessageEvent;
 import com.google.android.gms.wearable.WearableListenerService;
 
-/**
- * Created by jonathan on 7/8/15.
- */
-public class MyReceiverService extends WearableListenerService {
-    private static final String RECEIVER_SERVICE_PATH = "/receiver-service";
+public class WearListenerService extends WearableListenerService {
+    public WearListenerService() {
+    }
 
     @Override
     public void onCreate() {
-        super.onCreate();
-    }
 
-    String wearNodeId;
+        super.onCreate();
+        Log.i("WearListener", "The listener on the watch was created.");
+    }
 
     @Override
     public void onMessageReceived(MessageEvent messageEvent) {
-        wearNodeId = messageEvent.getSourceNodeId();
-        Log.i("Phone", "Phone received the message.");
-        Log.i("Wear", "nodeID is " + wearNodeId);
+        Log.i("WearListener", "Wear received the message back.");
         Intent i = new Intent();
-        i.putExtra("wearNodeId", wearNodeId);
-        i.setClass(this, PhotoWorkflowActivity.class);
+        i.setClass(this, MainActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(i);
     }
